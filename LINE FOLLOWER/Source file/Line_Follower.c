3be85f3fd10084490dae2cddd6e23ed3922a542a
TrulyDelulu <rafiqesler2004@gmail.com>
2026-05-18 14:14:23 +0800

    Update Line_Follower.c

--- LINE FOLLOWER/Source file/Line_Follower.c
+++ LINE FOLLOWER/Source file/Line_Follower.c
@@ -17,29 +17,34 @@ void main(void)
 
 	while (1)
 	{
+		sensor = 
 		switch (sensor)
 		{
+			//1010 forward
+			//1000 right
+			//0010 left
+			//1001 hard right
+			//0110 hard left
 			case 0x20: // 0010 0000 -> Center sensor only
-				PORTD = 0x05; // Go straight
+				PORTD = 0x0A; // Go straight
 				break;
 
 			case 0x40: // 0100 0000 -> Inner Left sensor
-				PORTD = 0x09; // Turn left (slight/soft turn)
+				PORTD = 0x02; // Turn left (slight/soft turn)
 				break;
 
 			case 0x80: // 1000 0000 -> Outer Left sensor
-				PORTD = 0x09; // Turn left (hard turn)
+				PORTD = 0x06; // Turn left (hard turn)
 				break;
 
 			case 0x10: // 0001 0000 -> Inner Right sensor
-				PORTD = 0x04; // Turn right (slight/soft turn)
+				PORTD = 0x08; // Turn right (slight/soft turn)
 				break;
 
 			case 0x08: // 0000 1000 -> Outer Right sensor
-				PORTD = 0x04; // Turn right (hard turn)
+				PORTD = 0x09; // Turn right (hard turn)
 				break;
 
-			case 0xF8: // 1111 1000 -> All 5 sensors active (T-junction or Cross)
 			case 0x00: // 0000 0000 -> No sensors active (Lost line)
 				PORTD = 0x00; // Stop
 				break;
@@ -47,7 +52,7 @@ void main(void)
 			default:
 				// Handles overlapping sensor reads (e.g., Center + Inner Left = 0x60)
 				// Defaulting to straight keeps the robot moving forward smoothly
-				PORTD = 0x05; // Go straight
+				PORTD = 0x0A; // Go straight
 				break;
 		}
 	}
